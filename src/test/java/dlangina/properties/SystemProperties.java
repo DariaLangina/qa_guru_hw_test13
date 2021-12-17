b7f792328b9be6fd1082ad0d92a9313f371ddc0c
DariaLangina <dlangina@rvision.ru>
2021-12-17 13:10:31 +0300

    properties tests

---
+++ src/test/java/dlangina/properties/SystemProperties.java
@@ -0,0 +1,20 @@
+package dlangina.properties;
+
+import static com.codeborne.selenide.Configuration.browser;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Configuration.browserVersion;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class SystemProperties {
+
+
+  @Test
+  @Tag("properties")
+  void someTest() {
+    System.out.println(browser);
+    System.out.println(browserVersion);
+    System.out.println(browserSize);
+  }
+}
