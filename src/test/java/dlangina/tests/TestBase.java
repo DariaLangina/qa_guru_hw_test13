e45896bed0869e0a7948b86e6f894dcda2cd527e
DariaLangina <dlangina@rvision.ru>
2021-12-17 16:42:11 +0300

    use system prperties + selenoid remove with credentials in file

--- src/test/java/dlangina/tests/TestBase.java
+++ src/test/java/dlangina/tests/TestBase.java
@@ -16,14 +16,14 @@ public class TestBase {
 
   @BeforeAll
   public static void config() {
-//    SelenoidConfig selenoid = ConfigFactory.create(SelenoidConfig.class);
+    SelenoidConfig selenoid = ConfigFactory.create(SelenoidConfig.class);
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
     Configuration.startMaximized = true;
-    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-//        Configuration.remote = format("https://%s:%s@%s", selenoid.login(),
-//                                      selenoid.password(), System.getProperty("selenoidUrl")
-//                                 );
+//    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = format("https://%s:%s@%s", selenoid.login(),
+                                      selenoid.password(), System.getProperty("selenoidUrl")
+                                 );
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     capabilities.setCapability("enableVNC", true);
