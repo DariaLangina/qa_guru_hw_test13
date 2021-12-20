279773c2b4fce1669d7e3bc55d7b269d930db8b9
DariaLangina <dlangina@rvision.ru>
2021-12-20 17:35:05 +0300

    deleted old

--- src/test/java/dlangina/tests/TestBase.java
+++ src/test/java/dlangina/tests/TestBase.java
@@ -1,9 +1,5 @@
 package dlangina.tests;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
-import static io.qameta.allure.Allure.step;
-
 import com.codeborne.selenide.logevents.SelenideLogger;
 import dlangina.helpers.Attach;
 import dlangina.helpers.DriverSettings;
@@ -20,16 +16,7 @@ public class TestBase {
   public static void setUp() {
     SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     DriverSettings.config();
-//    closeWeUseCookieModal();
   }
-
-  public static void closeWeUseCookieModal() {
-    step("Открытие главной страницы", () ->
-        open("https://new.rvision.ru/"));
-    step("Закрытие модального окна 'Мы используем Cookie'", () ->
-        $(".t-modal__close-cookie").click());
-  }
-
 
   @AfterEach
   public void tearDown() {
